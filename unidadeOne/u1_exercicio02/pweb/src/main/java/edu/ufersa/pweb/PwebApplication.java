b1fde3f531a2d0825082b794113457e04177bbe9
JV <joaovictorcunha12102002@gmail.com>
2024-02-17 19:06:44 -0300

    Parte um feita exercicio 2 - Organizaçao de pastas tambem

---
+++ unidadeOne/u1_exercicio02/pweb/src/main/java/edu/ufersa/pweb/PwebApplication.java
@@ -0,0 +1,13 @@
+package edu.ufersa.pweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebApplication.class, args);
+	}
+
+}
---
+++ unidadeOne/u1_exercicio02/pweb/src/test/java/edu/ufersa/pweb/PwebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ufersa.pweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
