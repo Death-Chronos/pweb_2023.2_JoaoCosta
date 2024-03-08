72fc5236713e2191cda74b1118d516b43b830376
JV <joaovictorcunha12102002@gmail.com>
2024-03-08 14:24:00 -0300

    criação do projeto

---
+++ unidadeTwo/roupa/src/main/java/pweb/roupa/RoupaApplication.java
@@ -0,0 +1,13 @@
+package pweb.roupa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoupaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoupaApplication.class, args);
+	}
+
+}
---
+++ unidadeTwo/roupa/src/test/java/pweb/roupa/RoupaApplicationTests.java
@@ -0,0 +1,13 @@
+package pweb.roupa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoupaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
