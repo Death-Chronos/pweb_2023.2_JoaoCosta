9baaa05aeaae7a24039a5b20bc33a2a98902def7
JV <joaovictorcunha12102002@gmail.com>
2024-03-08 20:58:15 -0300

    falta só editar

--- unidadeTwo/roupa/src/main/java/pweb/roupa/controllers/ClienteController.java
+++ unidadeTwo/roupa/src/main/java/pweb/roupa/controllers/ClienteController.java
@@ -3,17 +3,17 @@ package pweb.roupa.controllers;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import pweb.roupa.dtos.ClienteDTO;
 import pweb.roupa.entities.Cliente;
+import pweb.roupa.entities.composicao.Endereco;
+import pweb.roupa.entities.enums.Genero;
 import pweb.roupa.services.ClienteService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -28,11 +28,26 @@ public class ClienteController {
     public ModelAndView listarClientes() {
         return new ModelAndView("listaClientes", "clientes", cs.findAll());
     }
+
+    @GetMapping("/form")
+    public ModelAndView formCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setEndereco(new Endereco());
+        ModelAndView mv = new ModelAndView("formCliente");
+        mv.addObject("cliente", cliente);
+        mv.addObject("generos",Genero.values());
+
+
+        return mv;
+    }
+    
     
     @PostMapping("/salvar")
     public String salvarCliente(ClienteDTO dto) {
+        System.out.println(dto.email());
         Cliente cliente = new Cliente();
         BeanUtils.copyProperties(dto, cliente);
+        System.out.println(cliente.getEmail());
         cs.save(cliente);
         return "redirect:/clientes";
     }
